6dbef9d32d508ba8e5db9efff4fbb91e395192d1
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-12-29 12:51:48 +0600

    banner design done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import img1 from "../../assets/home/01.jpg";
+import img2 from "../../assets/home/02.jpg";
+import img3 from "../../assets/home/03.png";
+import img4 from "../../assets/home/04.jpg";
+import img5 from "../../assets/home/05.png";
+import img6 from "../../assets/home/06.png";
+
+const imgs = [img1, img2, img3, img4, img5, img6];
+
+const ONE_SECOND = 1000;
+const AUTO_DELAY = ONE_SECOND * 10;
+const DRAG_BUFFER = 50;
+
+const SPRING_OPTIONS = {
+  type: "spring",
+  mass: 3,
+  stiffness: 400,
+  damping: 50,
+};
 
 const Home = () => {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  const dragX = useMotionValue(0);
+
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      const x = dragX.get();
+
+      if (x === 0) {
+        setImgIndex((pv) => {
+          if (pv === imgs.length - 1) {
+            return 0;
+          }
+          return pv + 1;
+        });
+      }
+    }, AUTO_DELAY);
+
+    return () => clearInterval(intervalRef);
+  }, []);
+
+  const onDragEnd = () => {
+    const x = dragX.get();
+
+    if (x <= -DRAG_BUFFER && imgIndex < imgs.length - 1) {
+      setImgIndex((pv) => pv + 1);
+    } else if (x >= DRAG_BUFFER && imgIndex > 0) {
+      setImgIndex((pv) => pv - 1);
+    }
+  };
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="flex justify-center items-center h-screen">
+      <div className="relative overflow-hidden w-screen h-full">
+        <motion.div
+          drag="x"
+          dragConstraints={{
+            left: 0,
+            right: 0,
+          }}
+          style={{
+            x: dragX,
+          }}
+          animate={{
+            translateX: `-${imgIndex * 100}%`,
+          }}
+          transition={SPRING_OPTIONS}
+          onDragEnd={onDragEnd}
+          className="flex cursor-grab items-center active:cursor-grabbing w-full h-full"
+        >
+          <Images imgIndex={imgIndex} />
+        </motion.div>
+        <motion.div className="absolute bottom-20 left-0 right-0">
+          <Dots imgIndex={imgIndex} setImgIndex={setImgIndex} />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+const Images = ({ imgIndex }) => {
+  return (
+    <>
+      {imgs.map((imgSrc, idx) => {
+        console.log(`Loading image: ${imgSrc}`); // Debugging step
+        return (
+          <motion.div
+            key={idx}
+            style={{
+              backgroundImage: `url(${imgSrc})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            animate={{
+              scale: imgIndex === idx ? 1 : 1,
+            }}
+            transition={SPRING_OPTIONS}
+            className="aspect-video shrink-0 w-full h-screen bg-neutral-800 object-cover"
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const Dots = ({ imgIndex, setImgIndex }) => {
+  return (
+    <div className="mt-4 flex w-full justify-center gap-2">
+      {imgs.map((_, idx) => {
+        return (
+          <button
+            key={idx}
+            onClick={() => setImgIndex(idx)}
+            className={`h-3 w-3 rounded-full transition-colors ${
+              idx === imgIndex ? "bg-neutral-50" : "bg-neutral-500"
+            }`}
+          />
+        );
+      })}
     </div>
   );
 };
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const links = (
+    <>
+      <Link>Home</Link>
+      <Link>Food</Link>
+      <Link>About</Link>
+      <Link>Contact Us</Link>
+    </>
+  );
+
   return (
-    <div>
-      <h2>Nav</h2>
+    <div className="bg-[#0000]/25 fixed w-full z-50 py-4 text-white">
+      <div className="navbar md:w-11/12 lg:w-11/12 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm gap-6 dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow font-bold"
+            >
+              {links}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-end hidden lg:flex gap-16">
+          <ul className="menu menu-horizontal gap-6 px-1 font-extrabold text-lg">
+            {links}
+          </ul>
+          <a className="btn">Button</a>
+        </div>
+      </div>
     </div>
   );
 };
