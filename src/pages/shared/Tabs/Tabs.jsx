aa06471c8a5d4b180ae52a84438478f24b0895c4
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-12-31 11:39:41 +0600

    again tab redesign with responsiveness done

--- src/pages/shared/Tabs/Tabs.jsx
+++ src/pages/shared/Tabs/Tabs.jsx
@@ -1,77 +1,42 @@
-import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Tabs = ({ Tabs }) => {
+  const [selected, setSelected] = useState(Tabs[0]);
   return (
-    <div>
-      <div className="bg-neutral-100 lg:py-20 py-10">
-        <SlideTabs tabs={Tabs} />
-      </div>
+    <div className="px-4 my-10 py-4 rounded-full border-2 border-[#ffb710] flex text-lg items-center flex-wrap gap-2 w-fit mx-auto">
+      {Tabs.map((tab) => (
+        <Chip
+          text={tab}
+          selected={selected === tab}
+          setSelected={setSelected}
+          key={tab}
+        />
+      ))}
     </div>
   );
 };
 
+const Chip = ({ text, selected, setSelected }) => {
+  return (
+    <button
+      onClick={() => setSelected(text)}
+      className={`${
+        selected
+          ? "text-[#ffb300]"
+          : "text-black text-lg hover:text-black hover:text-[#f7c552f3] "
+      } text-xl uppercase font-bold transition-colors px-2.5 py-0.5 rounded-md relative`}
+    >
+      <span className="relative z-10">{text}</span>
+      {selected && (
+        <motion.div
+          layoutId="underline"
+          className="absolute bottom-0 left-[9px] right-[11px] h-[3px] bg-[#ffb300] rounded-md"
+          transition={{ type: "spring", duration: 0.5 }}
+        />
+      )}
+    </button>
+  );
+};
+
 export default Tabs;
-
-const SlideTabs = ({ tabs }) => {
-  const [position, setPosition] = useState({
-    left: 0,
-    width: 0,
-    opacity: 0,
-  });
-
-  return (
-    <ul
-      onMouseLeave={() => {
-        setPosition((pv) => ({
-          ...pv,
-          opacity: 0,
-        }));
-      }}
-      className="relative mx-auto flex flex-wrap justify-center w-11/12 lg:w-fit rounded-full border-2 border-[#dda727] bg-white p-1"
-    >
-      {tabs.map((tab, index) => (
-        <Tab key={index} setPosition={setPosition}>
-          {tab}
-        </Tab>
-      ))}
-
-      <Cursor position={position} />
-    </ul>
-  );
-};
-
-const Tab = ({ children, setPosition }) => {
-  const ref = useRef(null);
-
-  return (
-    <li
-      ref={ref}
-      onMouseEnter={() => {
-        if (!ref?.current) return;
-
-        const { width } = ref.current.getBoundingClientRect();
-
-        setPosition({
-          left: ref.current.offsetLeft,
-          width,
-          opacity: 1,
-        });
-      }}
-      className="relative z-10 block cursor-pointer px-[8px] lg:px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base"
-    >
-      {children}
-    </li>
-  );
-};
-
-const Cursor = ({ position }) => {
-  return (
-    <motion.li
-      animate={{
-        ...position,
-      }}
-      className="absolute z-0 h-7 rounded-full bg-[#ffb300] md:h-12 text-black"
-    />
-  );
-};
