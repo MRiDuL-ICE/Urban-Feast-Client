dbaed120962ac0a2ff0fd8452b04c441ddad450e
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-12-31 11:47:20 +0600

    shows the food card on shop with responsiveness done

--- src/pages/Shop/Shop/Shop.jsx
+++ src/pages/Shop/Shop/Shop.jsx
@@ -3,19 +3,25 @@ import Cover from "../../shared/Cover/Cover";
 import shopImg from "../../../assets/shop/banner2.jpg";
 import useMenu from "../../../Hooks/UseMenu";
 import Tabs from "../../shared/Tabs/Tabs";
+import MenuCard from "../../shared/MenuCard/MenuCard";
 
 const Shop = () => {
   const [menu] = useMenu();
   const category = [...new Set(menu.map((item) => item.category))];
 
   return (
-    <div>
+    <div className="lg:w-10/12 mx-auto w-10/12">
       <Cover
         img={shopImg}
         heading={"OUR SHOP"}
         subHeading={"Would you like to try a dish?"}
       ></Cover>
       <Tabs Tabs={category}></Tabs>
+      <div className="grid lg:grid-cols-4 gap-6 md:grid-cols-2 grid-cols-1s">
+        {menu.map((item, _id) => (
+          <MenuCard key={item._id} item={item}></MenuCard>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/shared/Tabs/Tabs.jsx
+++ src/pages/shared/Tabs/Tabs.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const Tabs = ({ Tabs }) => {
   const [selected, setSelected] = useState(Tabs[0]);
   return (
-    <div className="px-4 my-10 py-4 rounded-full border-2 border-[#ffb710] flex text-lg items-center flex-wrap gap-2 w-fit mx-auto">
+    <div className="px-4 my-10 py-1 lg:py-4 rounded-full border lg:border-2 border-[#ffb710] flex text-lg items-center justify-center flex-wrap gap-3 lg:w-fit w-11/12 mx-auto">
       {Tabs.map((tab) => (
         <Chip
           text={tab}
@@ -25,7 +25,7 @@ const Chip = ({ text, selected, setSelected }) => {
         selected
           ? "text-[#ffb300]"
           : "text-black text-lg hover:text-black hover:text-[#f7c552f3] "
-      } text-xl uppercase font-bold transition-colors px-2.5 py-0.5 rounded-md relative`}
+      } lg:text-xl text-sm uppercase font-bold transition-colors lg:px-2.5 py-0.5 rounded-md relative`}
     >
       <span className="relative z-10">{text}</span>
       {selected && (
