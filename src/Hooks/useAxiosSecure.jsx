ea83ca5267580d43b53fc2856a3b592ae9c8747d
MRiDuL-ICE <abdulwahab22400@gmail.com>
2025-01-05 17:08:39 +0600

    logout the user if token is invalid

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+import Swal from "sweetalert2";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
-  // request interceptor
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+
+  // request interceptor to add authorization header for every secure call to api
   axiosSecure.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem("access-token");
@@ -18,6 +23,28 @@ const useAxiosSecure = () => {
       return Promise.reject(error);
     }
   );
+
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await logOut();
+        Swal.fire({
+          title: status,
+          text: "Unauthorized Access",
+          icon: "error",
+        });
+        navigate("/signin");
+      }
+      console.log("status error in the interceptor", status);
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
