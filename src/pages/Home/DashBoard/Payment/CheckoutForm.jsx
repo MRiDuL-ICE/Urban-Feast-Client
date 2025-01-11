c8faa337c819ae3ecd0fa04511cfae73178c9932
MRiDuL-ICE <abdulwahab22400@gmail.com>
2025-01-11 11:20:42 +0600

    checkout form of stripe payment implemented

--- src/pages/Home/DashBoard/Payment/CheckoutForm.jsx
+++ src/pages/Home/DashBoard/Payment/CheckoutForm.jsx
@@ -1,9 +1,48 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import React from "react";
 
 const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+  };
+
   return (
     <div>
-      <h2>Checkout</h2>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        ></CardElement>
+        <button
+          className="px-6 mt-8 rounded-[3px] text-white bg-green-600 py-1"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
     </div>
   );
 };
--- src/pages/Home/DashBoard/Payment/Payment.jsx
+++ src/pages/Home/DashBoard/Payment/Payment.jsx
@@ -1,13 +1,20 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
+import CheckoutForm from "./CheckoutForm";
+import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
 
 // we have to add pulic key which we'll get from their website
-const stripePromise = loadStripe("");
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 const Payment = () => {
   return (
     <div>
-      <Elements stripe={stripePromise}></Elements>
+      <SectionTitle heading={"Payment"}></SectionTitle>
+      <div className="w-10/12 mx-auto bg-white h-[450px] p-24 rounded-md">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm></CheckoutForm>
+        </Elements>
+      </div>
     </div>
   );
 };
